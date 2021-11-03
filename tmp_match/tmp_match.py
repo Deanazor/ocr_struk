bbfa6737aacdca199ccb0b8c5d11293edd6dec5a
KristianW <zogojogo@gmail.com>
2021-11-03 11:41:56 +0700

    tmp match module

--- tmp_match/tmp_match.py
+++ tmp_match/tmp_match.py
@@ -16,8 +16,8 @@ def matching(dataset, input):
     #Iterate all templates and calculate text similarity
     jarowinkler = JaroWinkler()
     result=[jarowinkler.similarity(input.lower(), template.lower()) for template in templates]
+    
     #Prepare output
-
     prediction = templates[np.argmax(result)]
     confidence = result[np.argmax(result)]
     output = Output(prediction, confidence)
