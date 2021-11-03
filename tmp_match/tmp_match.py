01c096d424fbfb6081f4ab52dad6991b7cc3ca2b
KristianW <zogojogo@gmail.com>
2021-11-03 11:40:50 +0700

    template matching module & requirements

---
+++ tmp_match/tmp_match.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import collections
+from strsimpy.jaro_winkler import JaroWinkler
+
+Output = collections.namedtuple('Output', ['prediction', 'confidence'])
+
+def matching(dataset, input):
+    #Enroll templates to list
+    df = pd.read_csv(dataset)
+    templates = df['templates'].values.tolist()
+
+    #Input word
+    input = str(input)
+
+    #Iterate all templates and calculate text similarity
+    jarowinkler = JaroWinkler()
+    result=[jarowinkler.similarity(input.lower(), template.lower()) for template in templates]
+    #Prepare output
+
+    prediction = templates[np.argmax(result)]
+    confidence = result[np.argmax(result)]
+    output = Output(prediction, confidence)
+    
+    return output
